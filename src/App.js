1511ca1acc82bdd296f08cd60399de39e2f03c5a
rizki wahyudie git <rizkiwahyudie@gmail.com>
2022-04-01 14:32:39 +0700

    data karyawan

--- src/App.js
+++ src/App.js
@@ -6,16 +6,21 @@ class App extends Component {
     super(props);
     this.state = {
       dataApi: [],
+      // buat state untuk menyimpan post ke dalam database json kita
+      // inputan sesuai atribut dari json yaitu id, title, body
       dataPost: {
         id: 0,
-        title: "",
-        body: ""
+        namaKaryawan: "",
+        jabatan: "",
+        jenisKelamin: "",
+        date: ""
       },
       editPut: false
     }
     this.handleremove = this.handleremove.bind(this);
     this.reloadData = this.reloadData.bind(this);
     this.inputChange = this.inputChange.bind(this);
+    // tidak menggunakan bind krna menggunakan ero function
     // this.onSubmit = this.onSubmit.bind(this);
   }
 
@@ -70,14 +75,15 @@ class App extends Component {
       })
   }
 
-  // FUNCTION POST
+  // FUNCTION POST untuk menangkap data yang diinput dan dimasukkan ke data state
   inputChange(e) {
     // console.log(e.target.value);
     // menduplikasi atau mencopy kedalam variabel baru menggunakan isx
     let newDataPost = { ...this.state.dataPost };
-    // if merupakan logic untuk put
-    if(this.state.edit === false) {
-    newDataPost['id'] = new Date().getTime();
+    // if merupakan logic untuk put    
+    if (this.state.edit === false) {
+      // untuk membuat ID pakai otomatis getTime
+      newDataPost['id'] = new Date().getTime();
     }
     newDataPost[e.target.name] = e.target.value;
     this.setState({
@@ -107,19 +113,21 @@ class App extends Component {
           // AGAR SETELAH BUTTON SUBMIT DICLICK MAKA INPUTAN KOSONG VALUENYA
           let newDataPost = { ...this.state.dataPost };
           newDataPost['id'] = "";
-          newDataPost['title'] = "";
-          newDataPost['body'] = "";
+          newDataPost['namaKaryawan'] = "";
+          newDataPost['jabatan'] = "";
+          newDataPost['jenisKelamin'] = "";
+          newDataPost['date'] = "";
 
           this.setState({
             dataPost: newDataPost
           })
         })
     } else {
-      axios.put(`http://localhost:3004/posts/${this.state.dataPost.id}`,this.state.dataPost)
-      .then(() => {
-        this.reloadData();
-        this.clearData();
-      })
+      axios.put(`http://localhost:3004/posts/${this.state.dataPost.id}`, this.state.dataPost)
+        .then(() => {
+          this.reloadData();
+          this.clearData();
+        })
     }
   }
 
@@ -128,8 +136,10 @@ class App extends Component {
     // AGAR SETELAH BUTTON SUBMIT DICLICK MAKA INPUTAN KOSONG VALUENYA
     let newDataPost = { ...this.state.dataPost };
     newDataPost['id'] = "";
-    newDataPost['title'] = "";
-    newDataPost['body'] = "";
+    newDataPost['namaKaryawan'] = "";
+    newDataPost['jabatan'] = "";
+    newDataPost['jenisKelamin'] = "";
+    newDataPost['date'] = "";
 
     this.setState({
       dataPost: newDataPost
@@ -152,14 +162,18 @@ class App extends Component {
     return (
       <div>
         <p>Post</p>
-        <input type="text" value={this.state.dataPost.title} name="title" placeholder="Isi Title" onChange={this.inputChange} />
-        <input type="text" value={this.state.dataPost.body} name="body" placeholder="Isi Deskripsi" onChange={this.inputChange} />
+        <input type="text" value={this.state.dataPost.namaKaryawan} name="namaKaryawan" placeholder="Nama" onChange={this.inputChange} />
+        <input type="text" value={this.state.dataPost.jabatan} name="jabatan" placeholder="Jabatan" onChange={this.inputChange} />
+        <input type="text" value={this.state.dataPost.jenisKelamin} name="jenisKelamin" placeholder="Jenis Kelamin" onChange={this.inputChange} />
+        <input type="date" value={this.state.dataPost.date} name="date" onChange={this.inputChange} />
         <button type="submit" onClick={this.onSubmit}>SAVE</button>
         {this.state.dataApi.map((dat, index) => {
           return (
             <div key={index}>
-              <p>{dat.title}</p>
-              <p>{dat.body}</p>
+              <p>Nama : {dat.namaKaryawan}</p>
+              <p>Jabatan : {dat.jabatan}</p>
+              <p>Jenis Kelamin : {dat.jenisKelamin}</p>
+              <p>Tanggal Lahir : {dat.date}</p>
               <button value={dat.id} onClick={this.handleremove}>Delete</button>
               <button value={dat.id} onClick={this.getDataID}>Edit</button>
             </div>
